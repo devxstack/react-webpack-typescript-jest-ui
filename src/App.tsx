bf338f3288afbf95326687629e8f4841836fdd56
Coding Scale <codingscale157@gmail.com>
2025-05-02 22:37:40 +0530

    abc-123:feat:initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Simple Production Ready React + TypeScript + Webpack Boilerplate</h1>
+      <h1>React + TypeScript + Webpack + Jest Boilerplate</h1>
     </div>
   );
 };
